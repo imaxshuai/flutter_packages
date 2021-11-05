3710099314e136081c6ce9405ec23d4014f837cf
xushuai <229436890@qq.com>
2021-11-05 17:53:26 +0800

    back debug

--- s_utils copy/lib/src/commons/common.dart
+++
@@ -1,7 +0,0 @@
-const double Pi = 3.1415926;
-
-enum ViewStatus { init, loaded, error }
-
-enum StyleKey { one, two, three, four, five, six, eight, nine, def }
-
-enum ThemeKey { success, warning, danger, info }
